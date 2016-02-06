93eac07d59a72f563e7b159c2a65b92e4198877c
Jason welch <jason@jwelchdesign.com>
2016-02-06 00:34:59 -0800

    Let's add some tests in.

---
+++ tests/OceanCrest/EventsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class EventsTest extends \PHPUnit_Framework_TestCase
+{
+    
+    /**
+     * @test
+     */
+    function it_gathers_all_the_events_in_a_given_range()
+    {
+        $this->assertTrue(true, true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost:8000';
+}
