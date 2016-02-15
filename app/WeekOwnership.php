c50cc9c5341c8fe6e45edff352aba527904e6a4e
Jason welch <jason@jwelchdesign.com>
2016-02-14 21:58:34 -0800

    Refactored the classes a bit. Added comments. Cleaned up things in general.

--- app/WeekOwnership.php
+++ app/WeekOwnership.php
@@ -1,114 +1,247 @@
 <?php namespace OceanCrest;
 
+/*
+|--------------------------------------------------------------------------
+| Week Ownership
+|--------------------------------------------------------------------------
+|
+| This class is responsible for determining the ownership for the cabin
+| for given dates. The rules are as follows:
+| * Welch's get even weeks during even years. 
+| * Schu's get odd weeks during event years.
+| * Each each odd/even weeks swap sides. 
+| ** This means usually, side that ends the year, starts the year.
+| ** So a side gets two weeks there, but that will rotate each year.
+| * The side that had New Year's gets Memorial Day Weekend.
+| * The side that didn't get New Year's gets Fourth of July.
+| * The side that didn't get New Year's gets Labor Day Weekend.
+|
+*/
+
 class WeekOwnership {
 
-    protected $newYearsEven = 'schu';
-    protected $evenYearEvenWeek = 'welch';
-    protected $oddYearOddWeek = 'welch';
-    protected $oddYearEvenWeek = 'schu';
-
+    /**
+     * Given a date, get what week in the year it's in.
+     * 
+     * @param  int $day   
+     * @param  int $month 
+     * @param  int $year  
+     * @return int        
+     */
     public function getWeekNumber($day, $month, $year)
     {
         return date("W", mktime(0,0,0, $month, $day+3, $year));
     }
 
+    /**
+     * Determine which side is staying based on a given date.
+     * 
+     * @param  int $day   
+     * @param  int $month 
+     * @param  int $year  
+     * @return string
+     */
     public function determine($day, $month, $year)
     {
-        // Week Ownership
+        // Week number drives everything
         $weekNumber = $this->getWeekNumber($day, $month, $year);
        
-        // Welch's get fourth of july on even years
-        $fourthOfJulyWeek = $this->fourthOfJulyWeek($year);
-        if ($weekNumber == $fourthOfJulyWeek) {
-            if ($this->evenYear($year)) {
+        /*
+        |--------------------------------------------------------------------------
+        | Fourth of July
+        |--------------------------------------------------------------------------
+        |
+        | Whoever didn't have New Year's gets Fouth of July.
+        | Welch's get Fourth of July on even years.
+        | Schu's  get Fourth of July on odd years.
+        |
+        */
+        if ($this->isFourthOfJulyWeek($weekNumber, $year)) {
+            if ($this->even($year)) {
                 return 'welch';
             } else {
                 return 'schu';
             }
         }
 
-        // memorial day goes to the same side that had new years
-        // new years for welch == odd year
-        // new years for schu  == even year
-        $memorialDayWeek = $this->memorialDayWeek($year);
-        if ($weekNumber == $memorialDayWeek) {
-            if ($this->oddYear($year)) {
+        /*
+        |--------------------------------------------------------------------------
+        | Memorial Day Weekend - Last Monday in May
+        |--------------------------------------------------------------------------
+        |
+        | Whoever had New Year's, gets Memorial Day Weekend.
+        | Welch's get New Year's during odd years.
+        | Schu's  get New Year's during even years.
+        |
+        */
+        if ($this->isMemorialDayWeek($weekNumber, $year)) {
+
+            if ($this->odd($year)) {
                 return 'welch';
-            } else {
-                return 'schu';
-            }
+            } 
+            
+            return 'schu';
         }
 
-        // labor day goes to the opposite side that had new years
-        // new years for welch == odd year
-        // new years for schu  == even year 
-        $laborDayWeek = $this->laborDayWeek($year);
-        if ($weekNumber == $laborDayWeek) {
-            if ($this->evenYear($year)) {
+        /*
+        |--------------------------------------------------------------------------
+        | Labor Day Weekend - First Monday in September
+        |--------------------------------------------------------------------------
+        |
+        | Whoever didn't have New Year's gets Labor Day Weekend.
+        | Welch's get New Year's during odd years.
+        | Schu's  get New Year's during even years.
+        |
+        */
+        if ($this->isLaborDayWeek($weekNumber, $year)) {
+
+            if ($this->even($year)) {
                 return 'welch';
-            } else {
-                return 'schu';
-            }
+            } 
+            
+            return 'schu';
         }
 
-        // If the month is december and week is 1,
-        // then it's the next year. 
+        /**
+         * The last week of December could be the first week of the year. 
+         * If that is the case the even/odd year stuff needs to be for the year of that week.
+         * If we detect week 1 in December, then increase the year (even/odd) for the year of the week.
+         */
         if ($weekNumber == 1 && $month == 12) {
             $year++;
         }
 
-        // Along with alternating weeks each year
-        // Schu's get new years on even years
-        if ($year % 2 == 0) {
+        return $this->evenOddAssignment($weekNumber, $year);
+    }
 
-            // Schu's get ood weeks on even years so 
-            if ($weekNumber % 2 != 0) {
-                return 'schu';
-            } else {
-                return 'welch';
-            }
-
-        // odd year
-        } else {
-            
-            // welch's get odd weeks on odd years
-            if ($weekNumber % 2 != 0) {
-                return 'welch';
-            } else {
-                return 'schu';
-            }
+    /**
+     * Alternate weeks each year by assigning weeks to sides based on the 
+     * even/odd value of the current year.
+     * 
+     * @param  int $weekNumber 
+     * @param  int $year       
+     * @return string
+     */
+    public function evenOddAssignment($weekNumber, $year)
+    {
+        // odd years, welch's get odd weeks
+        if ($this->odd($year) && $this->odd($weekNumber)) {
+            return 'welch';
         }
+
+        // even year, welch's get even weeks
+        if ($this->even($year) && $this->even($weekNumber)) {
+            return 'welch';
+        }
+
+        // odd  year, schu's get even weeks
+        // even year, schu's get odd weeks
+        return 'schu';
     }
 
-    private function evenYear($year)
+    /**
+     * Whether a number is even or not.
+     * 
+     * @param  int $number
+     * @return boolean
+     */
+    private function even($number)
     {
-        return ($year % 2 == 0);
+        return ($number % 2 == 0);
     }
 
-    private function oddYear($year)
+    /**
+     * Whether a number is odd or not.
+     * 
+     * @param  int $number
+     * @return boolean
+     */
+    private function odd($number)
     {
-        return ! $this->evenYear($year);
+        return ! $this->even($number);
     }
 
+    /**
+     * Is the provided week a Fourth of July Week
+     * 
+     * @param  int  $weekNumber 
+     * @param  int  $year       
+     * @return boolean             [description]
+     */
+    public function isFourthOfJulyWeek($weekNumber, $year)
+    {
+        return ($weekNumber == $this->fourthOfJulyWeek($year));
+    }
+
+    /**
+     * Week number for the Fouth of July.
+     * 
+     * @param  int $year 
+     * @return int
+     */
     public function fourthOfJulyWeek($year)
     {
         $fourth = strtotime("july 4 $year");
         return date("W", mktime(0,0,0, date('m', $fourth), date('d', $fourth)+3, $year));
     }
 
+    /**
+     * Is the provided week a Memorial Day Week
+     * 
+     * @param  int  $weekNumber 
+     * @param  int  $year       
+     * @return boolean
+     */
+    public function isMemorialDayWeek($weekNumber, $year)
+    {
+        return ($weekNumber == $this->memorialDayWeek($year));
+    }
+
+    /**
+     * Week number Memorial Day is in.
+     * 
+     * @param  int $year 
+     * @return int       
+     */
     public function memorialDayWeek($year)
     {
         return $this->getWeekNumberFromTimestamp(strtotime("last monday of may $year"));
     }
 
+    /**
+     * Is the provided week a Labor Day Week
+     * 
+     * @param  int  $weekNumber 
+     * @param  int  $year       
+     * @return boolean
+     */
+    public function isLaborDayWeek($weekNumber, $year)
+    {
+        return ($weekNumber == $this->laborDayWeek($year));
+    }
+
+    /**
+     * Week number Labor Day is in.
+     * 
+     * @param  int $year 
+     * @return int       
+     */
     public function laborDayWeek($year)
     {
         return $this->getWeekNumberFromTimestamp(strtotime("first monday of september $year"));
     }
 
+    /**
+     * Week number for a given timestamp.
+     * @param  int $timestamp 
+     * @return int            
+     */
     private function getWeekNumberFromTimestamp($timestamp)
     {
-        return date("W", mktime(0,0,0, date('m', $timestamp), date('d', $timestamp)+3, date('Y', $timestamp)));
+        return date(
+            "W", 
+            mktime(0,0,0, date('m', $timestamp), date('d', $timestamp)+3, date('Y', $timestamp))
+        );
     }
 
 
--- app/WeekOwnershipSwap.php
+++ app/WeekOwnershipSwap.php
@@ -1,33 +1,67 @@
 <?php namespace OceanCrest;
 
+/*
+|--------------------------------------------------------------------------
+| Week Ownership Swap
+|--------------------------------------------------------------------------
+|
+| This class adds the additional functionality of swapping the week after
+| a normal week was stolen due to a holiday. This prevents a three-week
+| chunk for one side and creates two two-week chunks.
+|
+*/
+
 use Carbon\Carbon;
 
 class WeekOwnershipSwap extends WeekOwnership {
 
+    /**
+     * Determine which side is staying based on a given date.
+     * 
+     * @param  int $day   
+     * @param  int $month 
+     * @param  int $year  
+     * @return string
+     */
     public function determine($day, $month, $year)
     {
+        // With the default standard rules, who will get the cabin
         $standardFamily = parent::determine($day, $month, $year);
+        
         $date = Carbon::createFromDate($year, $month, $day);
-
         if ($this->previousWeekSwapped($date)) {
 
-            if ($standardFamily == 'welch') {
-                return 'schu';
+            // swap sides
+            if ($standardFamily != 'welch') {
+                return 'welch';
             }
-            return 'welch';
+            
+            return 'schu';
         }
 
         return $standardFamily;
     }
 
+    /**
+     * Determine if the previous week was swapped due to a holiday.
+     * @param  Carbon $date 
+     * @return boolean       
+     */
     private function previousWeekSwapped(Carbon $date)
     {
         // we want to check who had and should have add the cabin last week
         $date->subWeek();
 
-        $defaultFamily = $this->standard($date->day, $date->month, $date->year);
+        // Normal rules (even/odd) which side would get the cabin
+        $defaultFamily = $this->evenOddAssignment(
+            $this->getWeekNumber($date->day, $date->month, $date->year),
+            $date->year
+        );
+        
+        // With the holiday rules, who was the week given too
         $family = parent::determine($date->day, $date->month, $date->year);
 
+        // If they are different then they were swapped
         if ($defaultFamily != $family) {
             return true;
         }
@@ -35,31 +69,6 @@ class WeekOwnershipSwap extends WeekOwnership {
         return false;
     }
 
-    private function standard($day, $month, $year)
-    {
-        $weekNumber = $this->getWeekNumber($day, $month, $year);
-
-        // Along with alternating weeks each year
-        // Schu's get new years on even years
-        if ($year % 2 == 0) {
-
-            // Schu's get ood weeks on even years so 
-            if ($weekNumber % 2 != 0) {
-                return 'schu';
-            } else {
-                return 'welch';
-            }
-
-        // odd year
-        } else {
-            
-            // welch's get odd weeks on odd years
-            if ($weekNumber % 2 != 0) {
-                return 'welch';
-            } else {
-                return 'schu';
-            }
-        }
-    }
+    
 
 }
